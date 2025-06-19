7322de9b81a705980a36953a3b05759f4e2e4bad
Harathiyerramsetty <harathiyharathi@gmail.com>
2025-06-19 14:01:37 +0530

    Add files via upload

--- pattern/H1.java
+++ pattern/H1.java
@@ -206,7 +206,7 @@ INDI
 INDIA
 */
 
-
+/*j
 int size = 5;
 for (int i = 1; i <= size; i++)
 {
@@ -223,6 +223,159 @@ System.out.println();
 }
 }
 }
+* * * * *
+*          *
+*          *
+*          *
+* * * * *
+*/
+
+
+/*k
+int n = 4;
+for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+       *
+     * * *
+   * * * * *
+* * * * * * *
+   * * * * *
+     * * *
+        *
+*/
+/*l
+int n = 4;
+for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++)
+                System.out.print(" ");
+            System.out.print("*");
+           if (i != 0) {
+                for (int j = 0; j < 2 * i - 1; j++)
+                    System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++)
+
+                System.out.print(" ");
+             System.out.print("*");
+            if (i != 0) {
+                for (int j = 0; j < 2 * i - 1; j++)
+                    System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+   *
+  * *
+ *   *
+*     *
+ *   *
+  * *
+   *
+*/
+
+
+
+/*m
+int n = 7;
+ int mid = n / 2;
+ for (int i = 0; i < n; i++) {
+  for (int j = 0; j < n; j++) {
+  if (i == mid || j == mid) {
+  System.out.print("*");
+  } else {
+ System.out.print(" ");
+ }
+ }
+System.out.println();
+ }
+}
+}
+   *
+   *
+   *
+*******
+   *
+   *
+   *
+*/
+
+/*n
+ int n = 7;
+ for (int i = 1; i <= n; i++)
+ {
+ for (int j = 1; j <= n; j++)
+{
+ if (j == i || j == n - i + 1)
+System.out.print("*");
+else
+System.out.print(" ");
+}
+System.out.println();
+ }
+}
+}
+*     *
+ *   *
+  * *
+   *
+  * *
+ *   *
+*     *
+*/
+
+
+/*o
+int n = 5;
+for (int i = 0; i <= n; i++)
+{
+for (int j = 0; j < i; j++)
+System.out.print("*");
+System.out.println(i);
+}
+}
+}
+0
+*1
+**2
+***3
+****4
+*****5
+*/
+
+
+
+
+
+
+
+
+
+
+
 
 
 
